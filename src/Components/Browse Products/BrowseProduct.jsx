bfe20614567b3a2eb7d2d41a4c349855a2b9d3da
dilshadbasith <dilshadbasith01@gmail.com>
2024-03-04 17:21:30 -0800

    some updates

--- src/Components/Browse Products/BrowseProduct.jsx
+++ src/Components/Browse Products/BrowseProduct.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import bgImage from '../../assets/bg.jpg';
+import '../Browse Products/BrowseProduct.css';
 
 function BrowseProduct() {
   return (
-    <div>hooi</div>
-  )
+    <div className="bg-cover bg-center bg-no-repeat h-screen flex items-center justify-around flex-wrap" style={{ backgroundImage: `url(${bgImage})` }}>
+      <div className='card1'></div>
+      <div className='card1'></div>
+    </div>
+  );
 }
 
-export default BrowseProduct
+export default BrowseProduct;

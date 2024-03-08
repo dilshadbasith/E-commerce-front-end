ceb9163cd4540853179f5e37e072357e7c1303dc
dilshadbasith <dilshadbasith01@gmail.com>
2024-03-07 16:05:45 -0800

    some updates

--- src/Components/Browse Products/BrowseProduct.jsx
+++ src/Components/Browse Products/BrowseProduct.jsx
@@ -3,7 +3,8 @@ import bgImage from "../../assets/bg1.jpg";
 import "../Browse Products/BrowseProduct.css";
 import { useParams } from "react-router-dom";
 import { myContext } from "../Context";
-
+import { FaCartShopping } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
 function BrowseProduct() {
   const { id } = useParams();
   const { products } = useContext(myContext);
@@ -13,11 +14,31 @@ function BrowseProduct() {
   return (
     <div>
       {filtered.map((item) => (
-        <div className="bg-cover bg-center bg-no-repeat h-screen flex items-center justify-around flex-wrap"
-        style={{ backgroundImage: `url(${bgImage})` }}>
-          <div className="card1"><img src={item.src} alt="" className="h-[25rem]"/></div>
+        <div
+          className="bg-cover bg-center bg-no-repeat h-screen flex items-center justify-around flex-wrap"
+          style={{ backgroundImage: `url(${bgImage})` }}
+        >
           <div className="card1">
-            <h2 className="text-4xl text-white">{item.name}</h2>
+            <img src={item.src} alt="" className="h-[25rem]" />
+          </div>
+          <div className="card2">
+            <div className="flex justify-start items-center flex-col">
+              <h2 className="text-4xl text-white pt-3">{item.name}</h2>
+              <h2 className="text-2xl text-blue-800">{item.spec}</h2>
+              <h2 className="text-lg text-white p-4 text-center">
+                {item.spec1}
+              </h2>
+              <h2 className="text-xl text-red-600 font-bold pb-3 text-center line-through">
+                RS:{item.notemi}/-
+              </h2>
+              <h2 className="text-2xl text-blue-700 font-bold text-center">
+                NOW AT {item.emi}/-
+              </h2>
+            </div>
+            <div className="flex justify-around">
+              <button className="bg-red-700 p-3 font-bold text-white rounded-xl flex items-center gap-1"><FaHeart />Add To Wishlist</button>
+              <button className="bg-blue-700 pt-3 pb-3 font-bold pl-5 pr-5 text-white rounded-xl flex items-center gap-1"><FaCartShopping />Add To Cart</button>
+            </div>
           </div>
         </div>
       ))}
--- src/assets/Products.jsx
+++ src/assets/Products.jsx
@@ -6,48 +6,54 @@ export const productsdata=[
         name:"Boat Headphone 401",
         src:head1,
         spec:"ANC Supported",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"2100",
-        notemi:"2500,"
+        notemi:"2500"
     },
     {
         id:8,
         name:"Boat Headphone 401",
         src:head1,
         spec:"ANC Supported",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"2100",
-        notemi:"2500,"
+        notemi:"2500"
     },
     {
         id:9,
         name:"Boat Headphone 401",
         src:head1,
         spec:"ANC Supported",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"2100",
-        notemi:"2500,"
+        notemi:"2500"
     },
     {
         id:10,
         name:"Boat Headphone 401",
         src:head1,
         spec:"ANC Supported",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"2100",
-        notemi:"2500,"
+        notemi:"2500"
     },
     {
         id:11,
         name:"Boat Headphone 401",
         src:head1,
         spec:"ANC Supported",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"2100",
-        notemi:"2500,"
+        notemi:"2500"
     },
     {
         id:12,
         name:"Boat Headphone 401",
         src:head1,
         spec:"ANC Supported",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"2100",
-        notemi:"2500,"
+        notemi:"2500"
     },
     {
         id:1,
@@ -55,6 +61,7 @@ export const productsdata=[
         status:"hot",
         src:ip14,
         spec:"Dynamic Island",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"120000",
         notemi:"125000",
         qty:1
@@ -65,6 +72,7 @@ export const productsdata=[
         status:"hot",
         src:ip14,
         spec:"Dynamic Island",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"120000",
         notemi:"125000",
         qty:1
@@ -75,6 +83,7 @@ export const productsdata=[
         status:"hot",
         src:ip14,
         spec:"Dynamic Island",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"120000",
         notemi:"125000",
         qty:1
@@ -85,6 +94,7 @@ export const productsdata=[
         status:"hot",
         src:ip14,
         spec:"Dynamic Island",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"120000",
         notemi:"125000",
         qty:1
@@ -95,6 +105,7 @@ export const productsdata=[
         status:"hot",
         src:ip14,
         spec:"Dynamic Island",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"120000",
         notemi:"125000",
         qty:1
@@ -105,6 +116,7 @@ export const productsdata=[
         status:"hot",
         src:ip14,
         spec:"Dynamic Island",
+        spec1:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         emi:"120000",
         notemi:"125000",
         qty:1
